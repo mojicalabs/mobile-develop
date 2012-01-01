5e744bf0a6b9ebe59a50828a64b7c956002e7644
yamil.alburquerque <yamil.alburquerque@gmail.com>
2012-01-01 23:07:14 +0000

    Falta el edit de las entidades.

---
+++ shout_it-model/src/com/everydaymetrics/shout_it/managers/CredentialManager.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.everydaymetrics.shout_it.managers;
+import com.everydaymetrics.shout_it.controllers.JpaUtils;
+import com.everydaymetrics.shout_it.entities.Credential;
+import java.util.List;
+
+/**
+ *
+ * @author jalburquerque
+ */
+public class CredentialManager {
+    
+    private static CredentialManager instance;
+    
+    public static CredentialManager getCredentialManager() {
+        if (instance==null)
+        {
+            instance = new CredentialManager();
+            
+        }
+        
+        return instance;    
+    }
+    
+    public int createCredential(Credential credential) {
+        return JpaUtils.createEntity(credential);
+    }
+    
+    public boolean editCredential(Credential credential)
+    {
+        return JpaUtils.editEntity(credential);
+    }
+    
+    public List<Credential> getAllCredential() {
+        return JpaUtils.findAllEntities(new Credential());
+    }
+    
+    
+    
+}
